58eef39ebe17097eaf59969a64dddfbff7543ede
FeynmanNddbb <138034938+FeynmanNddbb@users.noreply.github.com>
2024-11-14 11:19:55 +0800

    Update sidebar.tsx
    change tt name

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -231,7 +231,7 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
+        title="FeynmanChat"
         subTitle="Build your own AI assistant."
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
